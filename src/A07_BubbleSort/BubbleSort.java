f016a833842ec157a75f873865e3a111bc6fee3f
Stefan <stefan@stefanraminger.at>
2017-04-28 20:33:39 +0200

    Fixed BubbleSort.sort

--- src/A07_BubbleSort/BubbleSort.java
+++ src/A07_BubbleSort/BubbleSort.java
@@ -7,27 +7,25 @@ public class BubbleSort implements PersonenSort {
 	 * Sortier-Funktion
 	 */
 	public void sort(Person[] personen) {
-
 		Person[] array = personen;
-		Person p ;
-		
+		Person p ;		
 		
 		boolean change = true;
 		
 		while(change){
-			change=false;
-			for (int i = 0; i < array.length-1; i++) {
-				if ( array[i].compareTo(array[i++])==1){
-					p = array[i];
-					array[i] = array[i++];
-					array[i++]=p;
-					change = true;
-					
-				}
-				
-			}
+			change = false;
 			
+			for (int i = 0; i < array.length-1; i++) {
+				if (array[i].compareTo(array[i + 1]) == 1) {
+					p = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = p;
+					change = true;					
+				}				
+			}			
 		}
+		
+		personen = array;
 	}
 	
 }
