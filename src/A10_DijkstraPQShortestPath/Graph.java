002623ddf29c63e931b031e0913b3bfee73ff7fb
Danijel Maric <danijel.maric@edu.campus02.at>
2017-05-11 16:10:36 +0200

    Update Graph.java

--- src/A10_DijkstraPQShortestPath/Graph.java
+++ src/A10_DijkstraPQShortestPath/Graph.java
@@ -3,7 +3,6 @@ import java.util.List;
 
 
 public interface Graph {
-
 	public int numVertices();
 	public boolean isDirected();
 	public boolean hasEdge(int u, int v);
@@ -11,5 +10,5 @@ public interface Graph {
 	public void addEdge(int u, int v);
 	public void addEdge(int u, int v, int weight);
 	public void removeEdge(int u, int v);
-	public List<WeightedEdge> getEdges(int v);
+	public List<WeightedEdge> getEdges(int v); 
 }
