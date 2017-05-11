ab903d095ef7f57bc801a3ed25a9fa17f2f8806d
Danijel Maric <danijel.maric@edu.campus02.at>
2017-05-11 16:08:43 +0200

    Update DijkstraPQShortestPath.java

--- src/A10_DijkstraPQShortestPath/DijkstraPQShortestPath.java
+++ src/A10_DijkstraPQShortestPath/DijkstraPQShortestPath.java
@@ -1,9 +1,10 @@
 package A10_DijkstraPQShortestPath;
+
 import java.util.List;
 
 public class DijkstraPQShortestPath extends FindWay {
 	private int[] dist;
-
+	
 	public DijkstraPQShortestPath(Graph graph) {
 		super(graph);
 	}
@@ -16,17 +17,38 @@ public class DijkstraPQShortestPath extends FindWay {
 		int numv = graph.numVertices();
 		dist = new int[numv];
 		for (int i=0; i < numv; i++) {
-			dist[i] = 9999;		// Summen im Graph dürfen nie mehr ergeben
+			dist[i] = 9999;		// Summen im Graph dÃ¼rfen nie mehr ergeben
 		}
 	}
 	
 	/**
-	 * Berechnet *alle* kürzesten Wege ausgehend vom Startknoten
-	 * Setzt dist[]- und pred[]-Arrays, kein Rückgabewert
+	 * Berechnet *alle* kÃ¼rzesten Wege ausgehend vom Startknoten
+	 * Setzt dist[]- und pred[]-Arrays, kein RÃ¼ckgabewert
 	 * @param from Startknoten
 	 */
 	protected boolean calculatePath(int from, int to) {
-
-		return false;
+		VertexHeap heap = new VertexHeap(graph.numVertices());
+		dist[from] = 0;
+		for (int i=0; i < graph.numVertices(); i++) {
+			heap.insert(new Vertex(i, dist[i]));
+		}
+		
+		while (!heap.isEmpty()) {
+			Vertex v = heap.remove();
+			if (v.vertex == to) {
+				return true;
+			}
+			List<WeightedEdge> edges = graph.getEdges(v.vertex);
+			for (WeightedEdge we: edges) {
+				//
+				int newDistance = dist[v.vertex] + we.weight;
+				if (newDistance < dist[we.to_vertex]) {
+					dist[we.to_vertex] = newDistance;
+					pred[we.to_vertex] = v.vertex;
+					heap.setCost(we.to_vertex, newDistance);
+				}
+			}
+		}
+		return (dist[to] != 9999);
 	}
 }
