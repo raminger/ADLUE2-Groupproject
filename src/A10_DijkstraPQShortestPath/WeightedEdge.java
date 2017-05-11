83cef122ee7bfab8578684c30c2adce0e24d5d64
Danijel Maric <danijel.maric@edu.campus02.at>
2017-05-11 16:13:45 +0200

    Update WeightedEdge.java

--- src/A10_DijkstraPQShortestPath/WeightedEdge.java
+++ src/A10_DijkstraPQShortestPath/WeightedEdge.java
@@ -10,5 +10,33 @@ public class WeightedEdge {
 		this.to_vertex = to_vertex;
 		this.weight = weight;
 	}
-	
+
+	public WeightedEdge(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getFrom_vertex() {
+		return from_vertex;
+	}
+
+	public void setFrom_vertex(int from_vertex) {
+		this.from_vertex = from_vertex;
+	}
+
+	public int getTo_vertex() {
+		return to_vertex;
+	}
+
+	public void setTo_vertex(int to_vertex) {
+		this.to_vertex = to_vertex;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}	
 }
+
