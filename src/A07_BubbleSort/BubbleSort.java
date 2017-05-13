339593dc71bde24962acacc2c09a423cf3ea844e
Stefan <stefan@stefanraminger.at>
2017-05-13 11:05:28 +0200

    check for greater equals in bubble sort

--- src/A07_BubbleSort/BubbleSort.java
+++ src/A07_BubbleSort/BubbleSort.java
@@ -16,7 +16,7 @@ public class BubbleSort implements PersonenSort {
 			changed = false;
 
 			for (int i = 0; i < array.length - 1; i++) {
-				if (array[i].compareTo(array[i + 1]) == 1) {
+				if (array[i].compareTo(array[i + 1]) >= 1) {
 					p = array[i];
 					array[i] = array[i + 1];
 					array[i + 1] = p;
